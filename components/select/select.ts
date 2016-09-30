263c403f4d4281aeeeba8c68b6e28b8867fa1b1d
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-09-30 13:21:56 +0300

    added rest of the highlight pipe usages

--- components/select/select.ts
+++ components/select/select.ts
@@ -143,7 +143,7 @@ let styles = `
                (mouseenter)="selectActive(o)"
                (click)="selectMatch(o, $event)">
             <a href="javascript:void(0)" class="dropdown-item">
-              <div [innerHtml]="sanitize(o.text) | highlight:inputValue"></div>
+              <div [innerHtml]="sanitize(o.text | highlight:inputValue)"></div>
             </a>
           </div>
         </li>
@@ -209,7 +209,7 @@ let styles = `
                (mouseenter)="selectActive(o)"
                (click)="selectMatch(o, $event)">
             <a href="javascript:void(0)" class="dropdown-item">
-              <div [innerHtml]="sanitize(o.text) | highlight:inputValue"></div>
+              <div [innerHtml]="sanitize(o.text | highlight:inputValue)"></div>
             </a>
           </div>
         </li>
@@ -228,7 +228,7 @@ let styles = `
                (click)="selectMatch(o, $event)"
                [ngClass]="{'active': isActive(o)}">
             <a href="javascript:void(0)" class="dropdown-item">
-              <div [innerHtml]="sanitize(o.text) | highlight:inputValue"></div>
+              <div [innerHtml]="sanitize(o.text | highlight:inputValue)"></div>
             </a>
           </div>
         </li>
