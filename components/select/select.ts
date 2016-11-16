b5a728e98fb93c725cf1ec2f81d51c27b1aa244b
SergeyKuryatnick <Sergey.kuryatnick@valor-software.com>
2016-11-16 16:20:21 +0200

    Revert "fix. Only one dropdown can be opened at the page in one period of time"
    This reverts commit e9b65fe07cf9fa4b11bbe05dcc4f8fe6f3b81af1.

--- components/select/select.ts
+++ components/select/select.ts
@@ -310,7 +310,6 @@ export class SelectComponent implements OnInit {
   private _items:Array<any> = [];
   private _disabled:boolean = false;
   private _active:Array<SelectItem> = [];
-  private isSelectedNow: boolean = false;
 
   public constructor(element:ElementRef, private sanitizer:DomSanitizer) {
     this.element = element;
@@ -357,17 +356,17 @@ export class SelectComponent implements OnInit {
       e.preventDefault();
     }
     // left
-    // if (!isUpMode && e.keyCode === 37 && this._items.length > 0) {
-    //   this.behavior.first();
-    //   e.preventDefault();
-    //   return;
-    // }
+    if (!isUpMode && e.keyCode === 37 && this._items.length > 0) {
+      this.behavior.first();
+      e.preventDefault();
+      return;
+    }
     // right
-    // if (!isUpMode && e.keyCode === 39 && this._items.length > 0) {
-    //   this.behavior.last();
-    //   e.preventDefault();
-    //   return;
-    // }
+    if (!isUpMode && e.keyCode === 39 && this._items.length > 0) {
+      this.behavior.last();
+      e.preventDefault();
+      return;
+    }
     // up
     if (!isUpMode && e.keyCode === 38) {
       this.behavior.prev();
@@ -426,11 +425,8 @@ export class SelectComponent implements OnInit {
   }
 
   public clickedOutside():void {
-    if (!this.isSelectedNow) {
-      this.inputMode = false;
-      this.optionsOpened = false;
-    }
-    this.isSelectedNow = false;
+    this.inputMode = false;
+    this.optionsOpened = false;
   }
 
   public get firstItemHasChildren():boolean {
@@ -438,7 +434,6 @@ export class SelectComponent implements OnInit {
   }
 
   protected matchClick(e:any):void {
-    this.isSelectedNow = true;
     if (this._disabled === true) {
       return;
     }
