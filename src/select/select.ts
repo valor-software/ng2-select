a96c00d9fba3aacaf652324db8e0fd7488493364
Phát Hồng <phat.hong@newoceaninfosys.com>
2017-09-14 11:25:55 +0700

    Update select.ts

--- src/select/select.ts
+++ src/select/select.ts
@@ -544,7 +544,7 @@ export class SelectComponent implements OnInit, ControlValueAccessor {
       .filter((option:SelectItem) => (this.multiple === false ||
       this.multiple === true && !this.active.find((o:SelectItem) => option.text === o.text)));
 
-    if (this.options.length > 0 && !this.behavior.activeOption) {
+    if (this.options.length > 0 && !(this.behavior as any).activeOption) {
       this.behavior.first();
     }
     this.optionsOpened = true;
