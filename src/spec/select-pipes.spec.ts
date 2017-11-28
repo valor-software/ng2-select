a7cd2fd6aee52d1aecbd69a432cfbe4f157cc6e2
Andrey Bolut <andrey.bolut@exposit.com>
2017-11-28 20:36:49 +0300

    fixed dropdowns

--- src/spec/select-pipes.spec.ts
+++ src/spec/select-pipes.spec.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SelectModule } from '../ng2-select';
+import { SelectModule } from '../ng2-select-exposit';
 
 const html = ``;
 
